99c7d9fe5833ac3796c03abd6bde5ee94083a3c0
SimranChaprana <simranchaprana77@gmail.com>
2026-06-05 23:42:18 +0530

    day1 leet2

--- src/week1/Day1/Leet2.java
+++ src/week1/Day1/Leet2.java
@@ -1,5 +1,28 @@
 package week1.Day1;
 
+import java.lang.classfile.components.ClassPrinter.ListNode;
+
+//Remove Duplicates from Sorted List
 public class Leet2 {
-    
+    public ListNode deleteDuplicates(ListNode head) {
+
+    ListNode prev = head;
+    ListNode temp = prev.next;
+
+    while (temp != null) {
+      if (temp.val == prev.val) {
+        temp = temp.next;
+        continue;
+      }
+
+      prev.next = temp;
+      prev = temp;
+      temp = temp.next;
+    }
+
+    prev.next = null;
+    return head;
+  }
+
 }
+
