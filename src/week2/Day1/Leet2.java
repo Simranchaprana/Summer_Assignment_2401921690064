6ad45725a3a9922462b8d126eee7f82190c2cdae
SimranChaprana <simranchaprana77@gmail.com>
2026-06-10 02:05:39 +0530

    dya 3 week 2

--- src/week2/Day1/Leet2.java
+++ src/week2/Day1/Leet2.java
@@ -1,5 +1,20 @@
 package week2.Day1;
 
 public class Leet2 {
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+
     
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
 }
--- src/week2/Day1/Leet3.java
+++ src/week2/Day1/Leet3.java
@@ -1,5 +1,23 @@
 package week2.Day1;
 
 public class Leet3 {
+    boolean canConstruct(String ransomNote, String magazine) {
+    int[] charCount = new int[26];
+
+    for (char c : magazine.toCharArray()) {
+      charCount[c - 'a']++;
+    }
+
     
+    for (char c : ransomNote.toCharArray()) {
+      if (charCount[c - 'a'] == 0) {
+        return false; 
+      }
+      charCount[c - 'a']--;
+    }
+
+    
+    return true;
+  }
+
 }
