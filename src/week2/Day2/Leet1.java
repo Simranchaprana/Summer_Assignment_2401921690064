b27240bf20e73fa78797dc1b873e7e7cdd0c38ba
SimranChaprana <simranchaprana77@gmail.com>
2026-06-09 23:19:33 +0530

    dya 2 week 2

---
+++ src/week2/Day2/Leet1.java
@@ -0,0 +1,30 @@
+package week2.Day2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Leet1 {
+
+    public int lengthOfLongestSubstring(String s) {
+    Set<Character> charSet = new HashSet<>();
+
+    int maxLength = 0;
+    int left = 0;
+
+    for (int right = 0; right < s.length(); right++) {
+
+      while (charSet.contains(s.charAt(right))) {
+        charSet.remove(s.charAt(left));
+        left++;
+      }
+
+      charSet.add(s.charAt(right));
+      maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    return maxLength;
+
+    }
+    
+    
+}
