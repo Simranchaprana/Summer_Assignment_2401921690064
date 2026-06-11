c8541d218de9865b1907899f16beeda222e80f33
SimranChaprana <simranchaprana77@gmail.com>
2026-06-11 22:00:34 +0530

    week2 day 5

--- src/week2/Day4/Leet3.java
+++ src/week2/Day4/Leet3.java
@@ -1,5 +1,29 @@
 package week2.Day4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Leet3 {
-    
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+
+    private void backtrack(List<String> result, String curr,
+                           int open, int close, int n) {
+
+        if (curr.length() == 2 * n) {
+            result.add(curr);
+            return;
+        }
+
+        if (open < n) {
+            backtrack(result, curr + "(", open + 1, close, n);
+        }
+
+        if (close < open) {
+            backtrack(result, curr + ")", open, close + 1, n);
+        }
+    }
 }
