b18adf265e34f514e4450eab1cf729ee81a18500
SimranChaprana <simranchaprana77@gmail.com>
2026-06-12 22:52:34 +0530

    week2 day 5

--- src/week2/Day5/Leet1.java
+++ src/week2/Day5/Leet1.java
@@ -1,6 +1,33 @@
 package week2.Day5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Leet1 {
-    
+    public List<List<String>> groupAnagrams(String[] strs) {
+    if (strs == null || strs.length == 0)
+      return new ArrayList<>();
+
+    Map<String, List<String>> stringAnagramsMap = new HashMap<>();
+    for (String s : strs) {
+      char[] arr = s.toCharArray();
+      Arrays.sort(arr);
+      String key = String.valueOf(arr);
+
+      if (!stringAnagramsMap.containsKey(key))
+        stringAnagramsMap.put(key, new ArrayList<>());
+
+      stringAnagramsMap.get(key).add(s);
+    }
+
+    List<List<String>> resultList = new ArrayList<>();
+    for (Map.Entry<String, List<String>> stringAnagrams : stringAnagramsMap.entrySet()) {
+      resultList.add(stringAnagrams.getValue());
+    }
+    return resultList;
+  }
 
 }
