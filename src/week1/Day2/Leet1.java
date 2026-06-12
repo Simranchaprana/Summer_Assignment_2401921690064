6645bebb1e209bc44fc2abf4845715384134bc5b
SimranChaprana <simranchaprana77@gmail.com>
2026-06-12 23:27:33 +0530

    week2 day 5

--- src/week1/Day2/Leet1.java
+++ src/week1/Day2/Leet1.java
@@ -1,5 +1,16 @@
 package week1.Day2;
 
 public class Leet1 {
+    public int maxSubArray(int[] nums) {
+    int max_so_far = nums[0];
+    int curr_max = nums[0];
+
+    for (int i = 1; i < nums.length; i++) {
+      curr_max = Math.max(nums[i], nums[i] + curr_max);
+      max_so_far = Math.max(curr_max, max_so_far);
+    }
+
+    return max_so_far;
+    }
     
 }
--- src/week1/Day2/Leet2.java
+++ src/week1/Day2/Leet2.java
@@ -1,5 +1,19 @@
 package week1.Day2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Leet2 {
+    public boolean containsDuplicate(int[] nums) {
+    Set<Integer> intSet = new HashSet<>();
+    for (int num : nums) {
+      if (intSet.contains(num))
+        return true;
+      intSet.add(num);
+    }
+
+    return false;
+  
+    }
     
 }
