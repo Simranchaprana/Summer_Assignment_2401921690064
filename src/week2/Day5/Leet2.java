da014bfb6d1d79fbf5f6b83de41ddd9e8356b79d
SimranChaprana <simranchaprana77@gmail.com>
2026-06-12 23:09:39 +0530

    week2 day 5

--- src/week2/Day5/Leet2.java
+++ src/week2/Day5/Leet2.java
@@ -1,5 +1,32 @@
 package week2.Day5;
 
 public class Leet2 {
-    
+    public int compress(char[] chars) {
+    StringBuilder sb=new StringBuilder();
+        int i=1;
+        int count =1;
+        sb.append(chars[0]);
+        while(i<chars.length){
+            if(chars[i]==chars[i-1]){
+                count++;
+            }
+            else{
+                if(count>1){
+                    sb.append(count);
+                }
+                sb.append(chars[i]);
+                count=1;            
+            }
+            i++;
+        }
+        if(count>1)
+        sb.append(count);
+
+        for(i=0;i<sb.length();i++){
+            chars[i]=sb.charAt(i);
+        }
+
+        return sb.length();
+
+    }
 }
