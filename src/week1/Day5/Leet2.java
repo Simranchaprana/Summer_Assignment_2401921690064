89419608bf41315db192453aad5160d23e80f03b
SimranChaprana <simranchaprana77@gmail.com>
2026-06-14 22:56:29 +0530

    .

--- src/week1/Day5/Leet2.java
+++ src/week1/Day5/Leet2.java
@@ -1,5 +1,17 @@
 package week1.Day5;
 
 public class Leet2 {
-    
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
 }
