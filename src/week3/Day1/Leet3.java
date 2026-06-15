104d1bfd4e43af8e73eb21c803548b3238b820c2
SimranChaprana <simranchaprana77@gmail.com>
2026-06-15 22:35:58 +0530

    week3 day 1

--- src/week3/Day1/Leet3.java
+++ src/week3/Day1/Leet3.java
@@ -1,5 +1,19 @@
 package week3.Day1;
 
+import java.lang.classfile.components.ClassPrinter.ListNode;
+
 public class Leet3 {
-    
+    public ListNode middleNode(ListNode head) {
+        ListNode slowPtr = head;
+    ListNode fastPtr = head;
+
+    while (fastPtr != null && fastPtr.next!= null) {
+
+      slowPtr = slowPtr.next;
+      fastPtr = fastPtr.next.next;
+    }
+
+    return slowPtr;
+
+    }
 }
