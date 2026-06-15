7a548635edc70d10f9d8a87c14c56303eed86ac5
SimranChaprana <simranchaprana77@gmail.com>
2026-06-15 22:34:03 +0530

    week3 day 1

--- src/week3/Day1/Leet2.java
+++ src/week3/Day1/Leet2.java
@@ -1,5 +1,20 @@
 package week3.Day1;
 
+import java.lang.classfile.components.ClassPrinter.ListNode;
+
 public class Leet2 {
+    public ListNode reverseList(ListNode head) {
     
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next; 
+            curr.next = prev;          
+            prev = curr;               
+            curr = next;              
+        }
+
+        return prev;
+    }
 }
