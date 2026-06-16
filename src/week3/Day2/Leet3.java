6cfeedc68ba57f8c33b81d65e909c2ef60b9287f
SimranChaprana <simranchaprana77@gmail.com>
2026-06-16 20:39:26 +0530

    leet3 week3 day2

--- src/week3/Day2/Leet3.java
+++ src/week3/Day2/Leet3.java
@@ -1,5 +1,38 @@
 package week3.Day2;
 
+import java.lang.classfile.components.ClassPrinter.ListNode;
+
 public class Leet3 {
-    
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head, slow = head;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+    if (fast != null) { // odd nodes: let right half smaller
+      slow = slow.next;
+    }
+
+
+    slow = reverseList(slow);
+    fast = head;
+    while (slow != null) {
+      if (fast.val != slow.val)
+        return false;
+
+      fast = fast.next;
+      slow = slow.next;
+    }
+    return true;
+  }
+    private ListNode reverseList(ListNode head) {
+    ListNode prev = null;
+    while (head != null) {
+      ListNode next = head.next;
+      head.next = prev;
+      prev = head;
+      head = next;
+    }
+    return prev;
+    }
 }
