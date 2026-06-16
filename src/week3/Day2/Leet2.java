4af9d532077d92aac97ec77eebf456a3b5bbdca4
SimranChaprana <simranchaprana77@gmail.com>
2026-06-16 20:29:03 +0530

    leet2 week3 day2

--- src/week3/Day2/Leet2.java
+++ src/week3/Day2/Leet2.java
@@ -1,5 +1,25 @@
 package week3.Day2;
 
+import java.lang.classfile.components.ClassPrinter.ListNode;
+
 public class Leet2 {
-    
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+       ListNode dummy = new ListNode(-1);
+    dummy.next = head;
+
+    ListNode firstPtr = dummy;
+    ListNode secondPtr = dummy;
+
+    for (int i = 0; i < n; i++) {
+      secondPtr = secondPtr.next;
+    }
+
+    while(secondPtr.next != null) {
+      firstPtr = firstPtr.next;
+      secondPtr = secondPtr.next;
+    }
+    firstPtr.next = firstPtr.next.next;
+
+    return dummy.next; 
+}
 }
