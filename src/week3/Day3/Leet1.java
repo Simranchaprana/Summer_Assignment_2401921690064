923c5ecaa61632c4a3baba244ed38ae8c95a89ea
SimranChaprana <simranchaprana77@gmail.com>
2026-06-17 20:56:31 +0530

    week3 day 3

--- src/week3/Day3/Leet1.java
+++ src/week3/Day3/Leet1.java
@@ -1,5 +1,23 @@
 package week3.Day3;
 
+import java.util.Stack;
+
 public  class Leet1 {
-    
+     public boolean isValid(String s) {
+       Stack<Character> stack = new Stack<>();
+
+    for (char c : s.toCharArray()) {
+
+      if (c == '(')
+        stack.push(')');
+      else if (c == '{')
+        stack.push('}');
+      else if (c == '[')
+        stack.push(']');
+
+      else if (stack.isEmpty() || stack.pop() != c)
+        return false;
+    }
+    return stack.isEmpty(); 
+    }
 }
