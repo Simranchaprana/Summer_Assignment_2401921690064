98d5b4becfb793e557aea48fbb7980bfc964015b
SimranChaprana <simranchaprana77@gmail.com>
2026-06-17 21:06:21 +0530

    week3 day 3

--- src/week3/Day3/Leet2.java
+++ src/week3/Day3/Leet2.java
@@ -1,5 +1,33 @@
 package week3.Day3;
 
+import java.util.Stack;
+
 public class Leet2 {
+    Stack<Integer> stack;
+     Stack<Integer> minStack;
+
+    public MinStack() {
+    stack = new Stack<>();
+    minStack = new Stack<>();
+    }
     
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek())
+      minStack.push(val);
+    }
+    
+    public void pop() {
+        int poppedValue = stack.pop();
+         if (poppedValue == minStack.peek())
+      minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
 }
