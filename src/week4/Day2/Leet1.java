55fed3903d3a5e5c10b8ad78d3339b8486528f35
SimranChaprana <simranchaprana77@gmail.com>
2026-06-23 22:46:07 +0530

    week4 day 2

--- src/week4/Day2/Leet1.java
+++ src/week4/Day2/Leet1.java
@@ -1,5 +1,31 @@
 package week4.Day2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import javax.swing.tree.TreeNode;
+
 public class Leet1 {
-    
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode curr = root;
+
+        while (curr != null || !stack.isEmpty()) {
+
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            curr = stack.pop();
+            result.add(curr.val);
+
+            curr = curr.right;
+        }
+
+        return result;
+    }
 }
