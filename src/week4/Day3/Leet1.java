8457f7d9f741a7ff153679bb030e3c5b10f34244
SimranChaprana <simranchaprana77@gmail.com>
2026-06-24 21:17:52 +0530

    week 4 day 3

--- src/week4/Day3/Leet1.java
+++ src/week4/Day3/Leet1.java
@@ -1,5 +1,20 @@
 package week4.Day3;
 
+import javax.swing.tree.TreeNode;
+
 public class Leet1 {
-    
+    public TreeNode searchBST(TreeNode root, int val) {
+                while (root != null) {
+            if (root.val == val) {
+                return root;
+            } else if (val < root.val) {
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+
+        return null;
+
+    }
 }
