bd4f0a7de8076f20f9095955a20e11ca645d4a8d
SimranChaprana <simranchaprana77@gmail.com>
2026-06-24 21:31:07 +0530

    week 4 day 3

--- src/week4/Day3/Leet3.java
+++ src/week4/Day3/Leet3.java
@@ -6,5 +6,19 @@ import java.util.List;
 import javax.swing.tree.TreeNode;
 
 public class Leet3 {
-    
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+      
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+
+        return (left != null) ? left : right;  
+    }
 }
