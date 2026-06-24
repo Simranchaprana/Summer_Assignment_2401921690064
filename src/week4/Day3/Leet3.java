e58d74d9296d8c8138955ff64a69b5e468ca247e
SimranChaprana <simranchaprana77@gmail.com>
2026-06-24 21:24:17 +0530

    week 4 day 3

--- src/week4/Day3/Leet3.java
+++ src/week4/Day3/Leet3.java
@@ -6,28 +6,5 @@ import java.util.List;
 import javax.swing.tree.TreeNode;
 
 public class Leet3 {
-    public boolean isValidBST(TreeNode root) {
-       List<Integer> inOrderList = new LinkedList<>();
-    helper(root, inOrderList);
-
-    boolean isBST = true;
-    int prev = inOrderList.get(0);
-    for (int i = 1; i < inOrderList.size(); i++) {
-      if (inOrderList.get(i) <= prev)
-        isBST = false;
-
-      prev = inOrderList.get(i);
-    }
-
-    return isBST;
-  }
-
-  void helper(TreeNode treeNode, List<Integer> inOrderList) {
-    if (treeNode == null)
-      return;
-
-    helper(treeNode.left, inOrderList);
-    inOrderList.add(treeNode.val);
-    helper(treeNode.right, inOrderList);
-    }
+    
 }
