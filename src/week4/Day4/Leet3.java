ee834bb362e06781eb69f8404e362a054d4837ce
SimranChaprana <simranchaprana77@gmail.com>
2026-06-25 22:14:00 +0530

    week 4 day 4

--- src/week4/Day4/Leet3.java
+++ src/week4/Day4/Leet3.java
@@ -1,5 +1,25 @@
 package week4.Day4;
 
+import javax.swing.tree.TreeNode;
+
 public class Leet3 {
-    
+    private int maxSum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return maxSum;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int left = Math.max(0, dfs(node.left));
+        int right = Math.max(0, dfs(node.right));
+
+        maxSum = Math.max(maxSum, node.val + left + right);
+
+        return node.val + Math.max(left, right);
+    }
 }
