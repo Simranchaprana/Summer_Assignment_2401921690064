3a00e0fcf5fc64f9f02100d4626c2239fcc8b3e8
SimranChaprana <simranchaprana77@gmail.com>
2026-06-26 22:08:21 +0530

    week 4 day 5

--- src/week4/Day5/Leet1.java
+++ src/week4/Day5/Leet1.java
@@ -1,5 +1,38 @@
 package week4.Day5;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
 public class Leet1 {
-    
+  public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+
+      Queue<TreeNode> leftTree = new LinkedList<>();
+      Queue<TreeNode> rightTree = new LinkedList<>();
+
+      leftTree.add(root.left);
+      rightTree.add(root.right);
+
+      while (!leftTree.isEmpty() && !rightTree.isEmpty()) {
+
+        TreeNode leftNode = leftTree.poll();
+        TreeNode rightNode = rightTree.poll();
+
+        if (leftNode == null && rightNode == null) continue;
+
+        if (leftNode == null || rightNode == null) return false;
+
+        if (leftNode.val != rightNode.val) return false;
+
+        // Pushing order is very important
+        leftTree.add(leftNode.left);
+        leftTree.add(leftNode.right);
+        rightTree.add(rightNode.right);
+        rightTree.add(rightNode.left);
+      }
+
+      return true;
+    }
 }
